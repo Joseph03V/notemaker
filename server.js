9b90d9ff147fa891145c5b4788766b8077fbeeef
Joey V <joey03v@gmail.com>
2023-12-08 13:59:30 -0600

    again some very non descriptive minor changes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const api_routes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use('/',html_routes)
+app.use('/api',api_routes)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
